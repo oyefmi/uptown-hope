57aed99ab00b0ac2b3b8ad4f46c92d811e6647c1
Michael <michael@MAC21-ISI-01.local>
2021-12-27 19:28:54 -0500

    Took out Timesheet button

--- src/pages/EmploymentTab/CurrentEmployee.js
+++ src/pages/EmploymentTab/CurrentEmployee.js
@@ -5,31 +5,27 @@ import './Employment.css'
 
 const CurrentEmployee = () => {
 
-    const [showTS, setShowTS] = useState(false);
+    // const [showTS, setShowTS] = useState(false);
     
-    const showTimesheet = () => {
-        setShowTS(true);
-    }
+    // const showTimesheet = () => {
+    //     setShowTS(true);
+    // }
 
     return (
         <div className="ce-employment">
             <div className="cover"></div>
                 <div className="wrapper">
                     <div className="page-content">
-                        <h2 id="title">Current Employees</h2>
-                        <p>
-                            <br />
-                            List of forms for Contractors:
-                            <br />
-                            <ul>
-                                <li></li>
-                            </ul>
-                        </p>
-                        <Button 
-                            variant="success" 
+                        <h2 id="title">Current Employees</h2><br />
+                        <h4 style={{textAlign: "center"}}>Timesheet</h4>
+
+                        <Timesheet />
+                        {/* <button
+                            className="tsButton"
                             type="button" 
-                            onClick={showTimesheet}>{ showTS ? <Timesheet /> : null } Fill Out Timesheet 
-                        </Button>
+                            onClick={showTimesheet}
+                            > { showTS ? <Timesheet /> : null } Fill Out Timesheet 
+                        </button> */}
                     </div>
             </div>
         </div>
